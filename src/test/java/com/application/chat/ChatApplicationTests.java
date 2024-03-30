ef3cf5ccb8666b3eddac291f271c569b19f7c4c8
Araf <arafullata@gmail.com>
2024-03-30 06:17:01 +0400

    Initial commit with bvsic java application with dependencies

---
+++ src/test/java/com/application/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
